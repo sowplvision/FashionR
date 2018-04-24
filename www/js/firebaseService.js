fd8bf7964d6cf4f0a9e382aaf4ea0268c0e0160e
AnnaZ <zuczekmine@gmail.com>
2018-04-24 15:09:11 +0200

    Dodano obsługę rejestracji, wylogowania. Poprawiono logowanie

--- www/js/firebaseService.js
+++ www/js/firebaseService.js
@@ -7,41 +7,69 @@
     		storageBucket: "fashionr-dad0c.appspot.com",
     		messagingSenderId: "623115610733"
   		};
-  	firebase.initializeApp(config);
+	  firebase.initializeApp(config);
+	}());
 	
-	$("#login").click(function() {
-		var email = $('#email').val();
-		var password = $('#password').val();
-		
-		firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		alert("Error: "+errorMessage);
+	$(document).ready(function(){
+		const emailInput = document.getElementById('emailLog');
+		const passInput = document.getElementById('passwordLog');
+		const emailRegInput = document.getElementById('emailReg');
+		const passwordReg = document.getElementById('passwordReg');
+		const passwordCheck = document.getElementById('passwordCheck');
+		const btnLogIn = document.getElementById('login');
+		const btnGoogleLogIn = document.getElementById('googleLogIn');
+		const btnFBLogIn = document.getElementById('facebookLogIn');
+		const btnRegis = document.getElementById('register');
+		const logout = document.getElementById('logout');
+	  
+		btnLogIn.addEventListener('click', function() {
+			const email = emailInput.value;
+			const passwd = passInput.value;
+			const auth = firebase.auth();
+			const promise = auth.signInWithEmailAndPassword(email, passwd);
+			promise.catch(function(error) {
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				alert("Error: "+errorMessage);
+				});
+			
 		});
-	});
-	
-	firebase.auth().onAuthStateChanged(firebaseUser => {
-	if (firebaseUser) {
-		console.log(firebaseUser);
-		$.mobile.changePage('#loggedInPage');
-	
-	} else {
-		console.log('Nie zalogowano!');
-	}
-	});
-	
-}());
+	  
+		btnRegis.addEventListener('click', function() {
 
+			const emailReg = emailRegInput.value;
+			const passwdReg1 = passwordReg.value;
+			const passwdReg2 = passwordCheck.value;
 
-function login() {
-	var email = $('#email').val();
-	var password = $('#password').val();
-	
-	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		alert("Error: "+errorMessage);
-	});
-	
-
-}
+			if(passwdReg1 === passwdReg2){
+				const auth = firebase.auth();
+				const promise = auth.createUserWithEmailAndPassword(emailReg, passwdReg1);
+				promise.catch(function(error) {
+					var errorCode = error.code;
+					var errorMessage = error.message;
+					alert("Error: "+errorMessage);
+					});
+				promise.then(function(){
+					$.mobile.changePage('#loggedInPage');
+				});
+	  
+			}else{
+				Alert("Hasła się różnią");		
+			}
+	  
+			
+		});
+	  
+		firebase.auth().onAuthStateChanged(firebaseUser => {
+			if(firebaseUser){
+				$.mobile.changePage('#loggedInPage');
+			}else{
+				$.mobile.changePage('#logInPage');
+			}
+		});
+	  
+		logout.addEventListener('click', function(){
+			firebase.auth().signOut();
+		});
+	  });
+	  
