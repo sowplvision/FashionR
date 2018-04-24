aa098fd50e1d7628be15c3ccb7789e9d051b6bd0
WWisla <wojciech.wislaa@gmail.com>
2018-04-24 16:01:40 +0200

    Dodanie logowanie Google+

--- www/js/firebaseService.js
+++ www/js/firebaseService.js
@@ -59,6 +59,18 @@
 	  
 			
 		});
+
+		btnGoogleLogIn.addEventListener('click', function() {
+			var provider = new firebase.auth.GoogleAuthProvider();
+			firebase.auth().signInWithPopup(provider).then(function(result) {
+				var user = result.user;
+				console.log(user);
+			}).catch(function(error) {
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				alert("Error: "+errorMessage);
+				});
+		});
 	  
 		firebase.auth().onAuthStateChanged(firebaseUser => {
 			if(firebaseUser){
