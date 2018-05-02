def06cf05ee4635855a5fd19ccafdc9701d24545
Daniel Kucharczyk <sowplvision@gmail.com>
2018-05-02 09:56:00 +0200

    Mały refactoring

--- www/js/scrapper.js
+++ www/js/scrapper.js
@@ -1,6 +1,6 @@
 var houseWomanCategories;
 var houseManCategories;
-var houseColorTable;
+//var houseColorTable;
 var croppWomanCategories;
 var croppManCategories;
 var womanCategory;
@@ -11,61 +11,26 @@ function scrapp(){
     //Tables with categories
     houseWomanCategories = [];
     houseManCategories = [];
-    houseColorTable = [];
+    //houseColorTable = [];
     croppWomanCategories = [];
     croppManCategories = [];
     womanCategory = [];
     manCategory = [];
     colorTable = ["inny","beżowy", "biały", "bordowy", "brązowy", "czarny", "czerwony", "kość słoniowa", "granatowy", "purpurowy", "niebieski", "pomarańczowy", "różowy", "srebrny", "szary", "turkusowy", "wielobarwny", "zielony", "złoty", "żółty", "", "surowy granatowy", "khaki"];
 
-    //scrappHouseClothing();
-    //scrappCropp();
-
     //Wait until all ajax functions will done
-    /**
     $.when(scrappHouseClothing(),scrappCropp()).done(function () {
         comparingCategories();
     });
-     */
 
-    getHouseOffers("http://www.house.pl/pl/pl/ona/kolekcja/bluzki-koszule");
-    getCroppOffers("https://www.cropp.com/pl/pl/chlopak/kolekcja/bluzy");
+    //getHouseOffers("http://www.house.pl/pl/pl/ona/kolekcja/bluzki-koszule");
+    //getCroppOffers("https://www.cropp.com/pl/pl/chlopak/kolekcja/bluzy");
 }
 
 function scrappHouseClothing(){
     /**
      *  GET Data from HOUSE website
      */
-    /**
-    $.get( "http://www.house.pl/pl/pl/", function( data ) {
-        console.log("\r\nHOUSE\r\n");
-        var i;
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(data, "text/html");
-
-        //Get Woman categories
-        console.log("\r\nWOMAN\r\n");
-        var womanCategories = doc.querySelectorAll("ul[id='womanCategory'] li[class='default'] a");
-        for (i = 0; i < womanCategories.length; i++){
-            if (!womanCategories[i].getAttribute("href").includes("sprawdz-to")) {
-                console.log(womanCategories[i].innerText + " " + womanCategories[i].getAttribute("href"));
-                //Add category to array
-                houseWomanCategories.push(womanCategories[i]);
-            }
-        }
-
-        //Get Man categories
-        console.log("\r\nMAN\r\n");
-        var manCategories = doc.querySelectorAll("ul[id='manCategory'] li[class='default'] a");
-        for (i = 0; i < manCategories.length; i++){
-            if (!manCategories[i].getAttribute("href").includes("sprawdz-to")) {
-                console.log(manCategories[i].innerText + " " + manCategories[i].getAttribute("href"));
-                //Add category to array
-                houseManCategories.push(manCategories[i]);
-            }
-        }
-    });
-    */
     return $.ajax({
         url:"http://www.house.pl/pl/pl/",
         dataType:"html",
@@ -107,32 +72,6 @@ function scrappCropp(){
     /**
      *  GET Data from CROPP website
      */
-    /**
-    $.get( "https://www.cropp.com/", function( data ) {
-        console.log("\r\nCROPP\r\n");
-        var i;
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(data, "text/html");
-
-        //Get Woman categories
-        console.log("\r\nWOMAN\r\n");
-        var womanCategories = doc.querySelectorAll("ul[class='level1'] li[class*='nav-1-3'] a");
-        for (i = 0; i < womanCategories.length; i++){
-            console.log(womanCategories[i].innerText + " " +  womanCategories[i].getAttribute("href"));
-            //Add category to array
-            croppWomanCategories.push(womanCategories[i]);
-        }
-
-        //Get Man categories
-        console.log("\r\nMAN\r\n");
-        var manCategories = doc.querySelectorAll("ul[class='level1'] li[class*='nav-2-3'] a");
-        for (i = 0; i < manCategories.length; i++){
-            console.log(manCategories[i].innerText + " " +  manCategories[i].getAttribute("href"));
-            //Add category to array
-            croppManCategories.push(manCategories[i]);
-        }
-    });
-     */
     return $.ajax({
         url:"https://www.cropp.com/",
         dataType:"html",
@@ -173,29 +112,18 @@ function comparingCategories() {
     console.log("CroppWomanCategories: " + croppWomanCategories.length);
     console.log("CroppManCategories: " + croppManCategories.length);
 
-    alert("SUCCESS");
-    //TODO made one standard for Woman and Man category
+    var str = "";
+    str += "{";
+
+
+
+    str += "}";
 }
 
 function getHouseOffers(url){
     /**
      * GET offers from House site (url)
      */
-    /**
-    $.get( url, function( data ) {
-        var i;
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(data, "text/html");
-
-        console.log("\r\nCATEGORY: " + url + "\r\n");
-
-        var offers = doc.querySelectorAll("div[id^='product-']");
-        for (i = 0; i < offers.length; i++){
-            console.log(offers[i]);
-        }
-        console.log(i);
-    });
-    */
     $.ajax({
         url:url,
         dataType:"html",
@@ -283,6 +211,7 @@ function convertHouseOfferToJSON(offer) {
 
     temp = offer.getAttribute("data-properties").split(",");
     var colorName = "\"color_name\":\"" + colorTable[temp[2].replace("\"color\":[\"","").replace("\"]","")] + "\"";
+
     /**
     if (temp[2].replace("\"color\":[\"","").replace("\"]","")==0){
         //colorName = "\"color_name\":\"" + temp[2].replace("\"color\":[\"","").replace("\"]","") + "\"";
@@ -292,7 +221,6 @@ function convertHouseOfferToJSON(offer) {
         colorName = "\"color_name\":" + houseColorTable[temp[2].replace("\"color\":[\"","").replace("\"]","")];
     }
      */
-    //console.log(houseColorTable[temp[2].replace("\"color\":[\"","").replace("\"]","")]);
     //console.log(colorName);
 
     temp = offer.querySelectorAll("span[class*='colorImg']");
