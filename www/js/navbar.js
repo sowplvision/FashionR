2b611fa4eec0246ca22c57f73a8b01558ae8c11c
WWisla <wojciech.wislaa@gmail.com>
2018-05-09 15:13:08 +0200

    Css

--- www/js/navbar.js
+++ www/js/navbar.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav(x) {
-    document.getElementById("mySidenav" + x).style.width = "45vw";
+    document.getElementById("mySidenav" + x).style.width = "100%";
     //x.classList.toggle("change");
 }
 
