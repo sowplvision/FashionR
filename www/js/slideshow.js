5ff722d18b651a6b1b30adcd93037d7672d235e7
WWisla <wojciech.wislaa@gmail.com>
2018-05-10 16:57:55 +0200

    Obsługa dots

--- www/js/slideshow.js
+++ www/js/slideshow.js
@@ -16,11 +16,16 @@ function currentDiv(n) {
 function showDivs(n) {
     var i;
     var x = document.querySelectorAll("div[class='offerGallery'] img");
+    var dots = document.getElementsByClassName("dots");
     if (n > x.length) {slideIndex = 1} 
     if (n < 1) {slideIndex = x.length}
     console.log(x.length);
     for (i = 0; i < x.length; i++) {
         x[i].style = "display:none;";
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].style = "background-color:transparent!important;";
+    }
     x[slideIndex-1].style = "display:block;";
+    dots[slideIndex-1].style = "background-color:rgba(0, 0, 0, 0.6)!important;";
 }
